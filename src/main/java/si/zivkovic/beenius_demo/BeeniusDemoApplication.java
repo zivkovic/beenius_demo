9ef3a08cf4658fdc6ca691e0ed5b3cafc7750c39
Jan Zivkovic <zivkovicjan@gmail.com>
2019-05-11 13:33:21 +0200

    Added intitial project setup with Spring Inizializr

---
+++ src/main/java/si/zivkovic/beenius_demo/BeeniusDemoApplication.java
@@ -0,0 +1,13 @@
+package si.zivkovic.beenius_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeniusDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeeniusDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/si/zivkovic/beenius_demo/BeeniusDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package si.zivkovic.beenius_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BeeniusDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
